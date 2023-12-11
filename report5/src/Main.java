0384a202d87f87c52528d5a19c39b0de9440a15a
Shimoji Yuto <e235729@ie.u-ryukyu.ac.jp>
2023-12-11 16:41:04 +0900

    second commit

--- report5/src/Main.java
+++ report5/src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
+        try{
         String str = null;
         System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.err.println("NullPointerExceptionが発生しました");
+            System.err.println(e.getMessage());
+        }
     }
 }
